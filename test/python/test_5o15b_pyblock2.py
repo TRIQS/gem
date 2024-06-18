6b66f2888f9c08af1748b8255d22b8b8f75197a4
Olivier Gingras <gingras.ol@gmail.com>
2024-06-18 19:28:27 -0400

    Adapted 5o15 test to EList.

--- test/python/test_5o15b_pyblock2.py
+++ test/python/test_5o15b_pyblock2.py
@@ -4,7 +4,8 @@ import unittest
 
 from triqs_ghostGA import LatticeSolver
 from triqs_ghostGA.grisb import *
-from triqs_ghostGA.utils_TH import get_semicircle_e_list, U_matrix_kanamori
+from triqs_ghostGA.utility.utils_TH import U_matrix_kanamori
+from triqs_ghostGA.utility.e_list import EList_SemiCircular
 from triqs.operators.util import U_matrix_kanamori as Umk
 import numpy as np
 from triqs_ghostGA.version import *
@@ -28,10 +29,10 @@ class test_hemb_5o15_pyblock2(unittest.TestCase):
         eloc[3,3] = tmp_e
 
         # construct ek with semicircular DOS
-        e_list = get_semicircle_e_list(nmesh=5000)
+        e_list = EList_SemiCircular(nmesh=5000).e_list
         eks = []
         for e in e_list:
-            tmp = np.array([[ 1.0*e, 0.0  ,   0.0,   0.0,   0.0], 
+            tmp = np.array([[ 1.0*e, 0.0  ,   0.0,   0.0,   0.0],
                             [   0.0, 1.0*e,   0.0,   0.0,   0.0],
                             [   0.0,   0.0, 1.0*e,   0.0,   0.0],
                             [   0.0,   0.0,   0.0, 1.0*e,   0.0],
