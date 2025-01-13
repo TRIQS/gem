a9f73dcfa8cde903e3f71d9475f39005220f1c3b
Olivier Gingras <gingras.ol@gmail.com>
2025-01-13 15:39:38 -0500

    Change which julia is used, for me at least. This is bad as there is an absolute link. Should be set up automatically, probably with CMake.

--- julia_init.sh
+++ julia_init.sh
@@ -1,12 +1,12 @@
 # module purge
 # module load triqs/3.2.x_nix2.2_llvm julia
 ###your julia binary
-export PYTHON_JULIAPKG_EXE="/mnt/sw/nix/store/fms6zspq4gnq3x8bps8i9wx4lhd6j8s3-julia-1.9.0/bin/julia"
+export PYTHON_JULIAPKG_EXE="/mnt/sw/nix/store/nypzid84k10gh1s6f68f555lka4l18k1-julia-1.9.3/bin/julia"
 ###the julia folder in ghostGA
-export PYTHON_JULIAPKG_PROJECT="/mnt/home/ogingras/Work/Libs_2.2-20230808/TRIQS/ghostGA/julia"
+export PYTHON_JULIAPKG_PROJECT="/mnt/home/ogingras/Work/Libs_2.3-20240529/TRIQS/ghostGA/julia"
 ##your python venv
 export JULIA_CONDAPKG_BACKEND="Null"
-export JULIA_PYTHONCALL_EXE="/mnt/home/ogingras/.py_3.9.15_2.1-20230222/bin/python"
+export JULIA_PYTHONCALL_EXE="/mnt/home/ogingras/.py_3.11.7_2.3-20240529/bin/python"
 # source /mnt/home/bkloss/projects/ghostGA/.triqs/bin/activate
 ###Threading layer defaults to sequential after sourcing these modules, so we set it to INTEL manually --- may be breaking things elsewhere!
 export MKL_THREADING_LAYER="INTEL"
@@ -14,5 +14,7 @@ export PYTHON_JULIACALL_HANDLE_SIGNALS=yes
 
 export MKL_NUM_THREADS=3
 export JULIA_NUM_THREADS=1
+
+export JULIA_LOAD_PATH=$JULIA_LOAD_PATH:$PYTHON_JULIAPKG_PROJECT/src
 
 # export JULIA_DEPOT_PATH="/mnt/home/bkloss/.julia/v1.9:/mnt/sw/nix/store/fms6zspq4gnq3x8bps8i9wx4lhd6j8s3-julia-1.9.0/local/share/julia:/mnt/sw/nix/store/fms6zspq4gnq3x8bps8i9wx4lhd6j8s3-julia-1.9.0/share/julia"
