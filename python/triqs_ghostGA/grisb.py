df25c583972551ba08917d6673e426278407f15a
Samuele Giuli <sgiuli@ccqlin067.flatironinstitute.org>
2026-01-13 08:15:21 -0500

    defining new run cycle

--- python/triqs_ghostGA/grisb.py
+++ python/triqs_ghostGA/grisb.py
@@ -369,6 +369,194 @@ class Grisb(object):
                         self.docc.append(self.edsolver.calc_double_occ(idx))
                     print("double occupancy=", self.docc)
                     break
+    # TODO: move sz_pen, etc to the solvers.
+    def run_gdmet(self, mu0=0.0, itmax=200, mix=0.5, tol=1e-6, beta=200., silence=True, idx=0, num_eig=2, ed_verbose=0, diis=False, nfix=None, dmu=0.1, mu_tol=1e-8, nfix_tol=0.01,regularisation=False):
+        """ Run ghost-RISB self-consistency
+
+        :param itmax: Maxiumum iteraction for self-consistency.
+        :type itmax: int
+
+        :param tol: Tolerence for convergence
+        :type tol: float
+
+        :param beta: Inverse temperature (equivalent to smearing temperature).
+        :type beta: float
+
+        :param silence: Silence the printing.
+        :type silence: bool
+
+        :param spin_pen: Penalty for S2 conservation.
+        :type spin_pen: float
+
+        :param silence: Orbital index for computing double occupancy.
+        :type idx: int
+
+        """
+
+        print("########## STARTING THE GHOST-GA LOOP ##########")
+
+        self.mu = mu0
+        self.diff = 1e20
+
+        if nfix is not None:
+            print("### Calculation performed in the canonical ensemble, starting with mu=", self.mu)
+
+        if diis is True:
+            #RDIIS = DIIS(7)
+            LDIIS = DIIS(7)
+            numNonDIIS = 4
+        for it in range(itmax):
+            print("### Iteration %d" % it)
+
+            # If number of electron is fixed, recalculate chemical potential to get the right number of electrons
+            if nfix is not None:
+                # Sometimes you want to start from a given chemical potential
+                if it > 1:
+                    # Target number of particle in the embedded space
+                    # TODO: I don't understand this
+                    nfix_qp = (self.nbath - self.nimp)/2 + nfix
+                    # Optimize to find chemical potential mu
+                    self.mu = find_mu(self.mu, self.R, self.Lambda, self.eks, nfix_qp, beta, dmu=dmu, mu_tol=mu_tol)
+
+            # From Lambda and R, calculate Delta_p, D and Lambda_c
+            print("# With Lambda and R, compute Delta_p, D and Lambda_c")
+            self.rhok_list = calc_rhoks(self.R, self.Lambda, self.eks, 1./beta, self.mu)
+            # self.rhok_list = np.real(calc_rhoks(self.R, self.Lambda, self.eks, 1./beta, self.mu))
+            self.Delta_p = calc_Delta_p(self.rhok_list)
+            # self.Delta_p = np.real(calc_Delta_p(self.rhok_list))
+            # self.D = np.real(calc_D(self.R, self.Lambda, self.Delta_p, self.eks, self.rhok_list))
+            if(regularisation):
+                self.space = measure_space(self.space,self.Delta_p)
+                self.D = calc_D_reg(self.R, self.Lambda, self.Delta_p, self.eks, self.rhok_list,self.space)    
+                self.Lambda_c = calc_Lambda_c_reg(self.R, self.Lambda, self.Delta_p, self.D, self.Hfull_list,self.space)
+            else:
+                self.D = calc_D(self.R, self.Lambda, self.Delta_p, self.eks, self.rhok_list)    
+                self.Lambda_c = calc_Lambda_c(self.R, self.Lambda, self.Delta_p, self.D, self.Hfull_list)
+            # self.Lambda_c = np.real(calc_Lambda_c(self.R, self.Lambda, self.Delta_p, self.D, self.Hfull_list))
+
+            # TODO: Nicer print and options for verbose
+            if not silence:
+                if self.spin_sym:
+                    print("Delta_p=")
+                    print(self.Delta_p[::2,::2])
+                    print("D=")
+                    print(self.D[::2,::2])
+                    print("Lambda_c=")
+                    print(self.Lambda_c[::2,::2])
+                    print()
+                else:
+                    print("Delta_p=")
+                    print(self.Delta_p[:,:])
+                    print("D=")
+                    print(self.D[:,:])
+                    print("Lambda_c=")
+                    print(self.Lambda_c[:,:])
+                    print()
+            sys.stdout.flush()
+
+            # With Lambda, R, Delta_p, D and Lambda_c, we have constructed the embedding Hamiltonian.
+            # We now solve with the solver passed as an argument.
+            print()
+            print("# Solving the embedding Hamiltonian:")
+            self.solve_embedding(self.mu, num_eig, ed_verbose)
+
+            # Extract relevant quantities from the density matrix, such as Delta_p
+            cdaggerf = self.denMat[:self.nimp,self.nimp:]
+            ffdagger = self.denMat[self.nimp:,self.nimp:]
+            ffdagger = (np.eye(self.nbath,dtype=np.complex128) - ffdagger).T
+            if not silence:
+                # Compare previous Delta_p with new
+                print("norm(ffdagger.T-Delta_p)=", np.linalg.norm(ffdagger.T-self.Delta_p))
+                print("new Delta_p =")
+                print(ffdagger.T)
+                print()
+            self.Delta_p = ffdagger.T
+            # self.Delta_p = np.real(ffdagger.T)
+
+            # TODO: Separate function
+            r"""Calculate R matrix, where the element are given by
+
+            .. math:
+                R_{b\alpha} = \sum_a \langle \Phi | c^\dagger_\alpha f_a | \Phi \rangle \left[ \Delta ( 1 - \Delta ) \right]^{-1/2}_{ad}
+            """
+            if(regularisation):
+                self.space = measure_space(self.space,self.Delta_p)
+                R_new = calc_R_reg(cdaggerf,self.Delta_p,self.space)
+                if self.spin_sym:
+                    R_new = np.kron(R_new[::2,::2],np.eye(2))# symmetrize
+                Lambda_new = calc_Lambda_reg(R_new, self.Lambda_c, self.Delta_p, self.D, self.Hfull_list,self.space)
+                if self.spin_sym:
+                    Lambda_new = np.kron(Lambda_new[::2,::2],np.eye(2)) # symmetryize
+            else:
+                R_new = np.transpose(cdaggerf.dot(funcMat(self.Delta_p, denR)))
+                if self.spin_sym:
+                    R_new = np.kron(R_new[::2,::2],np.eye(2))# symmetrize
+                Lambda_new = calc_Lambda(R_new, self.Lambda_c, self.Delta_p, self.D, self.Hfull_list)
+                if self.spin_sym:
+                    Lambda_new = np.kron(Lambda_new[::2,::2],np.eye(2)) # symmetryize
+
+            # Calculate difference of R and Lambda from prior iteration and check convergence
+            # and apply mixing if required
+            diff_R = np.abs(self.R-R_new).max()
+            diff_Lambda = np.abs(self.Lambda-Lambda_new).max()
+            self.diff = max(diff_R,diff_Lambda)
+            if diis and ( it >= numNonDIIS ):
+                error = Lambda_new - self.Lambda
+                error = np.reshape( error, error.shape[0]*error.shape[1] )
+                LDIIS.append( error, Lambda_new )
+                self.R = R_new#RDIIS.Solve()
+                self.Lambda = LDIIS.Solve()
+            else:
+                self.R = (1.-mix)*np.copy(self.R) + mix*R_new
+                self.Lambda = (1.-mix)*np.copy(self.Lambda) + mix*Lambda_new
+
+            if self.write is True:
+                # Save checkpoint
+                # TODO: Improve this
+                with HDFArchive('checkpoint%s.h5' % self.suff,'a') as fh5:
+                    fh5['R_%d' % it] = self.R
+                    fh5['Lambda_%d'% it] = self.Lambda
+                    fh5['eks'] = self.eks
+                    fh5['Utensor'] = self.Utensor
+                    fh5['mu'] = self.mu
+
+            if not silence:
+                print("R_new=")
+                print(R_new)
+                print("R=")
+                print(self.R)
+                print("Lambda_new=")
+                print(Lambda_new)
+                print("Lambda=")
+                print(self.Lambda)
+                print("ffdagger.T")
+                print(ffdagger.T)
+                print("density matrix=")
+                print(self.denMat)
+                print()
+
+            if nfix is not None:
+                # occ = occupation_vs_mu(self.mu, self.R, self.Lambda, self.eks, beta)
+                occ = np.trace(self.denMat[:self.nimp, :self.nimp])
+                print(self.denMat[:self.nimp, :self.nimp])
+                print("# New occupation : ", occ, ", nfix : ", nfix)
+                print()
+
+            print("# iteration:",it,'diff=',self.diff)
+            print()
+
+            # TODO: Add other criteria for convergence, example total energy
+            if self.diff < tol or it == (itmax-1):
+                if nfix is None or (nfix is not None and (occ - nfix < nfix_tol)):
+                    print("--------------------- ghost-RISB converged with diff=%g ---------------------"%(self.diff))
+                    print("density matrix=")
+                    print(self.denMat)
+                    self.nfill = np.trace(self.denMat[:self.nimp,:self.nimp])
+                    self.docc = []
+                    for idx in range(0,self.nimp,2):
+                        self.docc.append(self.edsolver.calc_double_occ(idx))
+                    print("double occupancy=", self.docc)
+                    break
 
     # def func_mu(self, mu, *args):
     #     #self.mu_tmp = mu
