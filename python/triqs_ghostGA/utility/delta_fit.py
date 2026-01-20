0cd376e04c7bd1167f448897454c66813b57ba9b
Samuele Giuli <samuele.giuli@gmail.com>
2026-01-20 15:32:39 -0500

    rebase due to divergence

--- python/triqs_ghostGA/utility/delta_fit.py
+++ python/triqs_ghostGA/utility/delta_fit.py
@@ -4,13 +4,13 @@ from scipy.optimize import root, least_squares, brentq, minimize
 # -------------------------
 # Build H
 # -------------------------
-def build_H(Lambda, Lambda_c, D, R):
-    H11 = Lambda
+def build_H(Lambda, Lambda_c, D, R, mu):
+    H11 = Lambda 
     H12 = R @ D.T
     H21 = H12.T.conj()
     H22 = -Lambda_c
     return np.block([[H11, H12],
-                     [H21, H22]])
+                     [H21, H22]]) -mu*np.eye(Lambda.shape[0]+Lambda_c.shape[0])
 
 # -------------------------------------------------
 # Scalar Fermi-like function (stable)
@@ -47,6 +47,7 @@ def get_mu(eps, beta):
 def F_of_H(H, beta):
     eps, U = np.linalg.eigh(H)
     mu   = get_mu(eps, beta)
+    mu=0.0# delete
     eps -= mu
     f = fermi(eps, beta)
     return (U * f) @ U.conj().T
@@ -57,6 +58,7 @@ def F_of_H(H, beta):
 def dF_spectral(H, dH, beta, tol=1e-14):
     eps, U = np.linalg.eigh(H)
     mu   = get_mu(eps, beta)
+    mu=0.0 # delete
     eps -= mu
 
     f = fermi(eps, beta)
@@ -67,10 +69,10 @@ def dF_spectral(H, dH, beta, tol=1e-14):
     eps_j = eps[None, :]
 
     G = np.zeros((len(eps), len(eps)))
+    G = np.broadcast_to(fp[:,None],G.shape) #adding derivatives of rows eigenvals
     mask = np.abs(eps_i - eps_j) > tol
-
+    # when not degenerate
     G[mask] = (f[:, None] - f[None, :])[mask] / (eps_i - eps_j)[mask]
-    np.fill_diagonal(G, fp)
 
     # rotate dH into eigenbasis
     dH_tilde = U.conj().T @ dH @ U
@@ -131,7 +133,7 @@ def dH_dD(dD, R):
 # Packing / unpacking parameters
 # It works for both Lambda+R and Lambda_c+D
 # ============================================================
-def pack_params(Lambda, R):
+def pack_params(Lambda, R, mu):
     n = R.shape[0]
     p = R.shape[1]
 
@@ -145,7 +147,8 @@ def pack_params(Lambda, R):
         Lam_re,
         Lam_im,
         R.real.reshape(-1),
-        R.imag.reshape(-1)
+        R.imag.reshape(-1),
+        np.array([mu]).real
     ])
 def unpack_params(x, n, p):
     iu = np.triu_indices(n)
@@ -170,7 +173,8 @@ def unpack_params(x, n, p):
     Lambda.imag[iu_strict] = Lam_im
     Lambda.imag[iu_strict[1],iu_strict[0]] = -Lam_im
     R = R_re + 1j * R_im
-    return Lambda, R
+    mu=x[-1]
+    return Lambda, R, mu
 
 
 # ============================================================
@@ -178,10 +182,10 @@ def unpack_params(x, n, p):
 # ============================================================
 def residual_LR(x, beta, Lambda_c, D, F22_target, RTF12_target):
     '''
-    Residual for Lambda+R fit.    
+    Residual for Lambda+R fit.
     '''
     n, p = D.shape
-    Lambda, R = unpack_params(x, n, p)
+    Lambda, R, mu = unpack_params(x, n, p)
 
     H = build_H(Lambda, Lambda_c, D, R)
     F = F_of_H(H, beta).T
@@ -189,6 +193,7 @@ def residual_LR(x, beta, Lambda_c, D, F22_target, RTF12_target):
     F22 = F[n:, n:]
     F12 = F[:n, n:]
     RTF12 = R.T@F12
+    dens_residue = np.sum(np.diag(F))-n
 
     iu = np.triu_indices(n)
     iu_strict = np.triu_indices(n, k=1)
@@ -198,7 +203,8 @@ def residual_LR(x, beta, Lambda_c, D, F22_target, RTF12_target):
         (F22.real[iu] - F22_target.real[iu]),
         (F22.imag[iu_strict] - F22_target.imag[iu_strict]),
         (RTF12.real - RTF12_target.real).ravel(),
-        (RTF12.imag - RTF12_target.imag).ravel()
+        (RTF12.imag - RTF12_target.imag).ravel(),
+        np.array([dens_residue])
     ])
     return res
 
@@ -207,14 +213,15 @@ def residual_LcD(x, beta, Lambda, R, F11_target, F12D_target):
     Residual for Lambda_c+D fit
     '''
     n, p = R.shape
-    Lambda_c, D = unpack_params(x, n, p)
+    Lambda_c, D, mu = unpack_params(x, n, p)
 
-    H = build_H(Lambda, Lambda_c, D, R)
+    H = build_H(Lambda, Lambda_c, D, R,mu)
     F = F_of_H(H, beta)
 
     F11 = F[:n, :n]
     F12 = F[:n, n:]
     F12D = F12@D
+    dens_residue = np.sum(np.diag(F))-n
 
     iu = np.triu_indices(n)
     iu_strict = np.triu_indices(n, k=1)
@@ -224,7 +231,8 @@ def residual_LcD(x, beta, Lambda, R, F11_target, F12D_target):
         (F11.real[iu] - F11_target.real[iu]),
         (F11.imag[iu_strict] - F11_target.imag[iu_strict]),
         (F12D.real - F12D_target.real).ravel(),
-        (F12D.imag - F12D_target.imag).ravel()
+        (F12D.imag - F12D_target.imag).ravel(),
+        np.array([dens_residue])
     ])
     return res
 
@@ -237,9 +245,9 @@ def jacobian_LR(x, beta, Lambda_c, D, F22_target, RTF12_target):
     Jacobian for Lambda+R fit
     '''
     n, p = D.shape
-    Lambda, R = unpack_params(x, n, p)
+    Lambda, R, mu = unpack_params(x, n, p)
 
-    H = build_H(Lambda, Lambda_c, D, R)
+    H = build_H(Lambda, Lambda_c, D, R, mu)
     F = F_of_H(H, beta)
 
     F22 = F[n:, n:]
@@ -251,17 +259,20 @@ def jacobian_LR(x, beta, Lambda_c, D, F22_target, RTF12_target):
     iu = np.triu_indices(n)
     iu_strict = np.triu_indices(n, k=1)
 
+    # dLambda and dR
     for k in range(len(x)):
         dx = np.zeros_like(x)
         dx[k] = 1.0
 
-        dLambda, dR = unpack_params(dx, n, p)
-        dH = dH_dLambda(dLambda, n) + dH_dR(dR, D)
+        dLambda, dR, dmu = unpack_params(dx, n, p)
+        dH = dH_dLambda(dLambda, n) + dH_dR(dR, D) - dmu*np.eye(2*n)
         dF = dF_spectral(H, dH, beta)
 
         dF22 = dF[n:, n:]
         dF12 = dF[:n, n:]
         dRTF12 = R.T@dF12 +dR.T@F12
+        
+        ddens = np.sum(np.diag(dF))
         
         # Must match the concatenation logic in residual()
         Jcols.append(np.concatenate([
@@ -270,7 +281,8 @@ def jacobian_LR(x, beta, Lambda_c, D, F22_target, RTF12_target):
             dF22.imag[iu_strict],
             # RTF12 (full)
             dRTF12.real.ravel(),
-            dRTF12.imag.ravel()
+            dRTF12.imag.ravel(),
+            np.array([ddens])
         ]))
 
     return np.column_stack(Jcols)
@@ -280,9 +292,9 @@ def jacobian_LcD(x, beta, Lambda, R, F11_target, F12D_target):
     Jacobian for Lambda_c+D
     '''
     n, p = R.shape
-    Lambda_c, D = unpack_params(x, n, p)
+    Lambda_c, D, mu = unpack_params(x, n, p)
 
-    H = build_H(Lambda, Lambda_c, D, R)
+    H = build_H(Lambda, Lambda_c, D, R, mu)
     F = F_of_H(H, beta)
 
     F11 = F[:n, :n]
@@ -298,13 +310,15 @@ def jacobian_LcD(x, beta, Lambda, R, F11_target, F12D_target):
         dx = np.zeros_like(x)
         dx[k] = 1.0
 
-        dLambda_c, dD = unpack_params(dx, n, p)
-        dH = dH_dLambdac(dLambda_c, n) + dH_dD(dD, R)
+        dLambda_c, dD, dmu = unpack_params(dx, n, p)
+        dH = dH_dLambdac(dLambda_c, n) + dH_dD(dD, R) - dmu*np.eye(2*n)
         dF = dF_spectral(H, dH, beta)
 
         dF11 = dF[:n, :n]
         dF12 = dF[:n, n:]
         dF12D = dF12@D +F12@dD
+        
+        ddens = np.sum(np.diag(dF))
         
         # Must match the concatenation logic in residual()
         Jcols.append(np.concatenate([
@@ -313,7 +327,8 @@ def jacobian_LcD(x, beta, Lambda, R, F11_target, F12D_target):
             dF11.imag[iu_strict],
             # F12D (full)
             dF12D.real.ravel(),
-            dF12D.imag.ravel()
+            dF12D.imag.ravel(),
+            np.array([ddens])
         ]))
 
     return np.column_stack(Jcols)
@@ -323,12 +338,12 @@ def jacobian_LcD(x, beta, Lambda, R, F11_target, F12D_target):
 # Root solve for Lambda and R (aka Self Energy)
 # ============================================================
 # This one works without derivatives and with least square instead of root
-def solve_F_only_LR(beta, Lambda_c, D, Lambda0, R0, F22_target, RTF12_target):
+def solve_F_only_LR(beta, Lambda_c, D, Lambda0, R0, mu0, F22_target, RTF12_target):
     '''
     Solve the root finding problem for a given set of Lambda_c,D,beta,F22=<badg b> and RTF12=R.T@<fdag b>
     using numerical derivatives
     '''
-    x0 = pack_params(Lambda0, R0)
+    x0 = pack_params(Lambda0, R0, mu0)
     # We use least_squares because it supports 'trf' and '2-point' (numerical) jacobians
     sol = least_squares(
         residual_LR,
@@ -342,19 +357,19 @@ def solve_F_only_LR(beta, Lambda_c, D, Lambda0, R0, F22_target, RTF12_target):
         verbose=2      # Useful to see if the cost function is actually decreasing
     )
     
-    Lambda_sol, R_sol = unpack_params(sol.x, Lambda0.shape[0], R0.shape[1])
-    return sol, Lambda_sol, R_sol
+    Lambda_sol, R_sol, mu_sol = unpack_params(sol.x, Lambda0.shape[0], R0.shape[1])
+    return sol, Lambda_sol, R_sol, mu_sol
 
 
 # This one works
-def solve_F_dF_LR(beta, Lambda_c, D, Lambda0, R0, F22_target, RTF12_target):
+def solve_F_dF_LR(beta, Lambda_c, D, Lambda0, R0, mu0, F22_target, RTF12_target):
     '''
     Solve the root finding problem for Lambda and R
     given set of Lambda_c,D,beta,F22=<badg b> and RTF12=R.T@<fdag b>
     using analytical derivatives.
     Least_squares() proved to be faster than root()
     '''
-    x0 = pack_params(Lambda0, R0)
+    x0 = pack_params(Lambda0, R0, mu0)
     if(True):
         sol = least_squares(
             residual_LR,
@@ -376,25 +391,25 @@ def solve_F_dF_LR(beta, Lambda_c, D, Lambda0, R0, F22_target, RTF12_target):
             method="lm", # Change from 'hybr' to 'lm'
             options={'ftol': 1e-9, 'xtol': 1e-9}
         )
-    Lambda_sol, R_sol = unpack_params(sol.x, Lambda0.shape[0], R0.shape[1])
-    return sol, Lambda_sol, R_sol
+    Lambda_sol, R_sol, mu_sol = unpack_params(sol.x, Lambda0.shape[0], R0.shape[1])
+    return sol, Lambda_sol, R_sol, mu_sol
 
-def new_self_energy( Lambda0,R0, Lambda_c,D, F22_target,RTF12_target, beta=200, method="dF"):
+def new_self_energy( mu0,Lambda0,R0, Lambda_c,D, F22_target,RTF12_target, beta=200, method="dF"):
     print("New self-energy fitting Lambda and R")
     if(method=="dF"):
-        res, new_Lambda, new_R = solve_F_dF_LR(beta, Lambda_c,D,Lambda0,R0,F22_target,RTF12_target)
+        res, new_Lambda, new_R, new_mu = solve_F_dF_LR(beta, Lambda_c,D,Lambda0,R0,mu0,F22_target,RTF12_target)
     elif(method=="F"):
-        res, new_Lambda, new_R = solve_F_only_LR(beta, Lambda_c,D,Lambda0,R0,F22_target,RTF12_target)
+        res, new_Lambda, new_R, new_mu = solve_F_only_LR(beta, Lambda_c,D,Lambda0,R0,mu0,F22_target,RTF12_target)
     else:
         raise ValueError(f"Tried new_self_energy with method={method} - only \"F\" and \"dF\" methods are available")
-    return new_Lambda, new_R
+    return new_Lambda, new_R, new_mu
 
 # ============================================================
 # Root solve for Lambda_c and D (aka Hybridization)
 # ============================================================
 # This one works without derivatives and with least square instead of root
-def solve_F_only_LcD(beta, Lambda, R, Lambda_c0, D0, F11_target, F12D_target):
-    x0 = pack_params(Lambda_c0, D0)
+def solve_F_only_LcD(beta, Lambda, R, Lambda_c0, D0, mu0, F11_target, F12D_target):
+    x0 = pack_params(Lambda_c0, D0, mu0)
     # We use least_squares because it supports 'trf' and '2-point' (numerical) jacobians
     sol = least_squares(
         residual_LcD,
@@ -407,19 +422,19 @@ def solve_F_only_LcD(beta, Lambda, R, Lambda_c0, D0, F11_target, F12D_target):
         ftol=1e-9,
         verbose=2      # Useful to see if the cost function is actually decreasing
     )
-    Lambda_c_sol, D_sol = unpack_params(sol.x, D0.shape[0], D0.shape[1])
-    return sol, Lambda_c_sol, D_sol
+    Lambda_c_sol, D_sol, mu_sol = unpack_params(sol.x, D0.shape[0], D0.shape[1])
+    return sol, Lambda_c_sol, D_sol, mu_sol
 
 
 # This one works
-def solve_F_dF_LcD(beta, Lambda, R, Lambda_c0, D0, F11_target, F12D_target):
+def solve_F_dF_LcD(beta, Lambda, R, Lambda_c0, D0, mu0, F11_target, F12D_target):
     '''
     Solve the root finding problem for Lambda_c and D
     given set of Lambda,R,beta,F11=<fadg f> and RTF12=<fdag b>@D
     using analytical derivatives.
     Least_squares() proved to be faster than root()
     '''
-    x0 = pack_params(Lambda_c0, D0)
+    x0 = pack_params(Lambda_c0, D0, mu0)
     if(True):
         sol = least_squares(
             residual_LcD,
@@ -441,20 +456,19 @@ def solve_F_dF_LcD(beta, Lambda, R, Lambda_c0, D0, F11_target, F12D_target):
             method="lm", # Change from 'hybr' to 'lm'
             options={'ftol': 1e-9, 'xtol': 1e-9}
         )
-    Lambda_c_sol, D_sol = unpack_params(sol.x, D0.shape[0], D0.shape[1])
-    return sol, Lambda_c_sol, D_sol
+    Lambda_c_sol, D_sol, mu_sol = unpack_params(sol.x, D0.shape[0], D0.shape[1])
+    return sol, Lambda_c_sol, D_sol, mu_sol
 
 
-def new_hybridization( Lambda_c0,D0, Lambda,R, F11_target, F12D_target, beta=200, method="dF"):
+def new_hybridization( mu0,Lambda_c0,D0, Lambda,R, F11_target, F12D_target, beta=200, method="dF"):
     print("New hybridization fitting Lambda_c and D")
     if(method=="dF"):
-        res, new_Lambda_c, new_D = solve_F_dF_LcD(beta, Lambda,R,Lambda_c0,D0,F11_target,F12D_target)
+        res, new_Lambda_c, new_D, new_mu = solve_F_dF_LcD(beta, Lambda,R,Lambda_c0,D0,mu0,F11_target,F12D_target)
     elif(method=="F"):
-        res, new_Lambda_c, new_D = solve_F_only_LcD(beta, Lambda,R,Lambda_c0,D0,F11_target,F12D_target)
+        res, new_Lambda_c, new_D, new_mu = solve_F_only_LcD(beta, Lambda,R,Lambda_c0,D0,mu0,F11_target,F12D_target)
     else:
         raise ValueError(f"Tried new_hybridization with method={method} - only \"F\" and \"dF\" methods are available")
-    
-    return new_Lambda_c, new_D
+    return new_Lambda_c, new_D, new_mu
    
 
 def residual_minimize(x, beta, Lambda_c, D, F22_target, RTF12_target):
