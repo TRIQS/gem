18836ca6c57e184b59ad98c1a13ca64c977a631c
Samuele Giuli <samuele.giuli@gmail.com>
2026-02-04 17:02:08 -0500

    delta_fit withou mu and utils_grisb with new Lambda_c routine"

--- python/triqs_ghostGA/utility/utils_grisb.py
+++ python/triqs_ghostGA/utility/utils_grisb.py
@@ -82,6 +82,43 @@ def calc_Lambda_c(R, Lambda, Delta_p, D, H_list):
     Lambda_c = realHcombination(lc, H_list)
     return Lambda_c
 
+def calc_Lambda_c_new(R, Lambda, Delta_p, D, H_list):
+    r"""Compute Lambda_c matrix from R, D, Lambda and Delta. It is given by
+
+    .. math::
+        \Lambda_c = \sum_s l^c_s h_s
+
+    where :math:`h_s` are a set of matrices that forms a basis for the space of Hermitian matrices
+    with dimension :math:`\nu`. The elements :math:`l^c_s` are obtained by calculating
+
+    .. math::
+        l^c_s = -l_s - \sum_{\alpha} \text{Tr} \left[ \frac{\partial}{\partial d^P_s} \left[ \Delta ( 1 - \Delta ) ]^{1/2} ( D R^T ) \right]
+
+    where :math:`l_s` are the elements of the decomposition of :math:`\Lambda = \sum_s l_s h_s`
+    and :math:`d^P_s` of :math:`\Delta_p = \sum_s d_s h_s^T`.
+
+    Here, extracting the :math:`l_s`s from :math:`\Lambda` is done using the inverse_realHcombination function.
+    The derivative with respect to :math:`d^P_s` is done using the dF function.
+    :math:`\Lambda_c` is reconstructed using the realHcombinaison function.
+
+    """
+    d, dvecs = np.linalg.eigh(Delta_p.T)
+    left_d= dvecs @ np.diag(np.sqrt(d/(1-d))) @ dvecs.T.conj()
+    right_d= dvecs @ np.diag(np.sqrt((1-d)/d)) @ dvecs.T.conj()
+    Ltmp = 0.5* left_d @ Lambda @ right_d
+    Ltmp += Ltmp.T.conj()
+    l = inverse_realHcombination(Ltmp, H_list)
+    lc = np.copy(l) * 0.0
+    DR = np.dot(D, np.transpose(R))
+    for k in range(len(H_list)):
+        AA = Delta_p
+        HH = H_list[k].T
+        derivative = dF(AA, HH, denRm1, ddenRm1)
+        tt = np.trace(np.dot(DR ,derivative))
+        lc[k] = -l[k] - (tt + np.conjugate(tt)).real
+    Lambda_c = realHcombination(lc, H_list)
+    return Lambda_c
+
 #########################################
 
 def calc_Lambda(R, Lambda_c, Delta_p, D, H_list):
