2279814286d2aa158c9b589a45a393aa5e9e9670
Samuele Giuli <samuele.giuli@gmail.com>
2026-05-07 14:51:47 -0400

    Updated solver template

--- python/triqs_ghostGA/solvers/solver_template.py
+++ python/triqs_ghostGA/solvers/solver_template.py
@@ -3,7 +3,7 @@
 # Author: Samuele Giuli
 # Email:  samuele.giuli@gmail.com
 #######################################################
-
+import numpy as np
 
 class SolverTemplate(object):
     '''
@@ -40,7 +40,7 @@ class SolverTemplate(object):
         self.Zpart = None # MANDATORY: partition function for thermal calculations divided by exp(gs_ene/T) so that is 1 at zero temperature
         print("solve_Hemb not implemented yet")
         #HERE YOU SHOULD SOLVE
-        
+
 
     
     def calc_density_matrix(self):
@@ -50,18 +50,22 @@ class SolverTemplate(object):
           denmat: numpy.array. Densty matrix, <c^\dagger_i c_j>, of the system (impurity+bath).
         '''
         print("calc_density_matrix not implemented yet")
+        denMat = None
+        return denMat
 
     
-    def compute_E1loc(self):
+    def compute_E1loc(self,eloc,mu=0.0):
         '''
         Compute the local one-body energy E1loc = Tr[eloc * denmat_impurity]
         Return:
           E1loc: float. Local one-body energy.
         '''
-        print("compute_E1loc not implemented yet")
-
+        # THIS IS A POSSIBLE IMPLEMENTATION GIVEN THE DENSITY MATRIX AND THE LOCAL ONE-BODY HAMILTONIAN
+        denMat = self.calc_density_matrix()
+        E1loc = np.trace((eloc - mu * np.eye(self.nimp)).dot(denMat[:self.nimp,:self.nimp].T))
+        return E1loc
     
-    def compute_E2loc(self):
+    def compute_E2loc(self,eloc,D,Lambdac,mu=0.0):
         '''
         Compute local energy including local one and two-body term from a given set od thermal states
         Works also at zero Temperature
@@ -70,5 +74,14 @@ class SolverTemplate(object):
           Eloc: float. Total local energy.
         '''
         print("compute_E2loc not implemented yet")
+        #HERE IS A POSSIBLE IMPLEMENTATION GIVEN THE DENSITY MATRIX, THE ONE-BODY HAMILTONIAN AND THE GROUND STATE ENERGY
+        denMat = self.calc_density_matrix()
+        E1tot = 0.0
+        E1tot+=np.trace((eloc - mu * np.eye(self.nimp)).dot(denMat[:self.nimp,:self.nimp].T)) #eloc part
+        E1tot+=np.trace(Lambdac.dot(denMat[self.nimp:,:self.nimp].T)) #bath part
+        E1tot+=np.trace(D.dot(denMat[self.nimp:,self.nimp:].T)) #hybridization part
+        E1tot+=np.trace(D.T.conjg().dot(denMat[:self.nimp,self.nimp:].T)) #hybridization part
+        E2loc = self.gs_ene - E1tot
+        return E2loc
 
     
